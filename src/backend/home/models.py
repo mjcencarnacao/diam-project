1809d87cec06dfb165219eff6ffe7e9fd8b667bb
Miguel Encarnação <mjceo@iscte-iul.pt>
2022-04-12 15:47:30 +0100

    [DIAM-02] Comment Scrapper
    º Added Comment Scrapper
    º Removed Res Folder

--- src/backend/home/models.py
+++ src/backend/home/models.py
@@ -8,6 +8,7 @@ class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     raw = models.JSONField()
     entry = models.DateTimeField(auto_now_add=True)
+    img = models.TextField(max_length=250)
 
     def __str__(self) -> str:
         return self.title
--- src/backend/services/scrapper.py
+++ src/backend/services/scrapper.py
@@ -1,12 +1,17 @@
 import json
 import requests
-from home import models
+from bs4 import BeautifulSoup
 
 class Scrapper:
 
     @staticmethod
     def get_movie_top(url='https://raw.githubusercontent.com/theapache64/top250/master/top250_min.json'):
         request = json.loads(requests.get(url).text)
-            #[models.Movie.objects.create(raw = movie).save() for movie in request]
         return request
-        
+
+    @staticmethod
+    def get_movie_comments(url:str) -> dict:
+        soup = BeautifulSoup(requests.get(url).text, 'lxml')
+        titles = [a.text for a in soup.select('a.title')]
+        descriptions = [a.text for a in soup.select('div.text')]
+        return dict(zip(titles, descriptions))
