b9aa887aaceac1e695a23bc7d9da62087b54ff4b
Shina69 <pedroliveira_92@hotmail.com>
2022-05-07 21:01:22 +0100

    Fixed logout/login bug; signout button revived

--- src/backend/members/views.py
+++ src/backend/members/views.py
@@ -52,4 +52,19 @@ def home(request):
 
 def logout_user(request):
     logout(request)
-    return render(request, 'authenticate/login.html')
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home:home_page')
+            else:
+                messages.success(request, "Invalid Credentials")
+                return redirect('members:login_user')
+    else:
+        msg = 'error validation form'
+        return render(request, 'authenticate/login.html', {'form': form})
