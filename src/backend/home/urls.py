31a426b3fff0bb62e17a024c344ad28a0f58ecd7
Miguel Encarnação <mjceo@iscte-iul.pt>
2022-05-09 18:47:33 +0100

    Navbar Fix
    º Fixed Navbar highlight

--- src/backend/home/urls.py
+++ src/backend/home/urls.py
@@ -5,7 +5,7 @@ from members import views as member_views
 # URLConf
 app_name = 'home'
 urlpatterns = [
-    path('', views.get_home_page, name= 'home_page'),
+    path('top/', views.get_home_page, name= 'home_page'),
     path('details/<movie_id>', views.movie_details, name= 'movie-details'),
     path('like/', views.like, name='like'),
     path('dislike/', views.dislike, name='dislike'),
