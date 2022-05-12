853d96eed97a81158dcbf63bdb33d0c85f05ba3d
Folgado <jfolgado94@gmail.com>
2022-05-12 21:36:30 +0100

    likes ok

--- src/backend/home/templatetags.py
+++ src/backend/home/templatetags.py
@@ -1,17 +1,23 @@
 from django.template.defaulttags import register
-from .models import Comments
+from .models import Comments, Movie
+
+
+@register.filter
+def get_movie_likes(movie: Movie):
+    return movie.likes.count()
 
 
 @register.simple_tag
 def count_movie_comments(movie_id, user_id):
     comments_counter: int = Comments.objects.filter(movie_id=movie_id, critic_id=user_id).count()
-    print("comments number on movie with id = " + str(movie_id) + " -> " + str(comments_counter))
     return comments_counter
+
 
 @register.simple_tag
 def next_tag(lista, arg1):
     print("comment_id - " + str(arg1) + " nota - " + str(lista[lista.index(arg1) + 1]))
     return lista[lista.index(arg1) + 1]
+
 
 @register.filter
 def get_movie_name(dictionary):
@@ -32,12 +38,14 @@ def get_movie_year(dictionary):
 def get_movie_description(dictionary):
     return dictionary.get('desc')
 
+
 @register.filter
 def get_movie_genre(dictionary):
     genre = dictionary.get('genre')
-    return  str(genre).translate({ord(c): None for c in '[]\''})
+    return str(genre).translate({ord(c): None for c in '[]\''})
+
 
 @register.filter
 def get_movie_actors(dictionary):
     genre = dictionary.get('actors')
-    return  str(genre).translate({ord(c): None for c in '[]\''})
+    return str(genre).translate({ord(c): None for c in '[]\''})
--- src/backend/home/urls.py
+++ src/backend/home/urls.py
@@ -6,12 +6,13 @@ from members import views as member_views
 app_name = 'home'
 urlpatterns = [
     path('', views.get_home_page, name='home_page'),
-    path('top/', views.get_home_page, name= 'home_page'),
-    path('details/<movie_id>', views.movie_details, name= 'movie-details'),
+    path('top/', views.get_home_page, name='home_page'),
+    path('details/<movie_id>', views.movie_details, name='movie-details'),
+    path('like_movie/<int:pk>', views.like_movie, name="like-movie"),
     path('like/', views.like, name='like'),
     path('dislike/', views.dislike, name='dislike'),
     path('register', views.get_register_page, name='register'),
     path('search', views.search_movies, name='movie-search'),
-    path('evaluate/', views.get_eval, name= 'eval'),
+    path('evaluate/', views.get_eval, name='eval'),
     path('erase/', views.erase, name='erase'),
 ]
